51df584b0b8764770902f009b1d7aedb865c313f
Iskra Marinova <imarinova@icginfo.com>
2019-10-25 11:25:10 -0700

    Initialization of SpringBoot CRUD API

---
+++ user-manager-crud-api/src/main/java/com/usermanager/springboot/usermanagercrudapi/UserManagerCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.usermanager.springboot.usermanagercrudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagerCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagerCrudApiApplication.class, args);
+	}
+
+}
---
+++ user-manager-crud-api/src/test/java/com/usermanager/springboot/usermanagercrudapi/UserManagerCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usermanager.springboot.usermanagercrudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagerCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
